340e19e92db65876e6d535def49b58fd901f93e6
Saar <saar9550@gmail.com>
2022-04-05 19:17:42 +0300

    ex3

---
+++ Ex2/mymodule.py
@@ -0,0 +1,23 @@
+
+
+
+
+def sum(x: int):
+    """
+    this func sum of given integer
+    :param x:
+    :return x+x:
+    """
+    return x + x
+
+def power(x: int):
+    return x ** 2
+
+def concatenation(x: str):
+    return x + x
+
+def subtraction_by_1(x: float):
+    return x-1
+
+def slice(string:str, start: int, end: int) -> str:
+    return string[start:end]
---
+++ Ex3/README.md
@@ -0,0 +1,8 @@
+# Research-algorithms_ex2
+
+
+
+## q4:
+https://www.codingame.com/profile/c0a51b1602d5f97d2e13d6d94e3eaf9b5648674
+
+![image](https://user-images.githubusercontent.com/20986238/157329216-a5bef2c1-019a-4038-8c38-d3b17ddc3bd2.png)
