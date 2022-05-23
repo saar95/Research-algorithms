de31f2a23b9874f2b54ce8055609e7b773a10f75
Saar <saar9550@gmail.com>
2022-05-23 20:28:58 +0300

    Q2 finished

---
+++ Ex8/Q2.py
@@ -0,0 +1,35 @@
+import gspread
+import numpy as np
+
+
+def sheet_to_list(sheet):
+    matrix = []
+    for i in sheet:
+        temp_l = []
+        for j in i:
+            temp_l.append(int(j))
+        matrix.append(temp_l)
+    return matrix
+
+
+def multiply_matrices_algorithm(A,B):
+    account = gspread.service_account("multiply-matrices-3c860f58740b.json")
+    spreadsheet = account.open("Multiply matrices")
+    a_matrix = spreadsheet.worksheet(A)
+    b_matrix = spreadsheet.worksheet(B)
+    A_val = a_matrix.get_all_values()
+    B_val = b_matrix.get_all_values()
+    matrix_A = np.array(sheet_to_list(A_val))
+    matrix_B = np.array(sheet_to_list(B_val))
+    result = np.dot(matrix_A, matrix_B)
+    title = A+"_times_"+B
+    spreadsheet.add_worksheet(title=title, rows=result.shape[0], cols=result.shape[1])
+    result_matrix = spreadsheet.worksheet(title)
+    last_cell = chr(ord('A') + (result.shape[1] - 1)) + str(result.shape[0])
+    sheet_to_write = "A1:" + last_cell
+    result_matrix.update(sheet_to_write, result.tolist())
+
+if __name__ == '__main__':
+    multiply_matrices_algorithm("A","B")
+    multiply_matrices_algorithm("C","D")
+
